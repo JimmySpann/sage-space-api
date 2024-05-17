0f52b139509b4390745af36624a12c020c2e6254
Jimmy Spann <jimmyleespann@outlook.com>
2024-05-16 22:13:59 -0400

    update cors

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
-app.use(cors({
-  origin: '*', // process.env.NODE_ENV === 'local' ? ['http://localhost:3000'] : ['https://sage-space.jimmyleespann.com'],
-  methods: "GET,POST,PUT,DELETE",
-  // credentials: true, // allows the session cookie to be sent back and forth from server to client
-  optionsSuccessStatus: 200 // some legacy browsers choke on satus 204
-}));
-app.options('*', cors());
+app.use(cors());
 
 // middleware - JSON parsing
 app.use(express.urlencoded({extended: false}));
