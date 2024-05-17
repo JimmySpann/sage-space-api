2cad9b5b2a334898ea696de08a9d107aea18de47
Jimmy Spann <jimmyleespann@outlook.com>
2024-05-16 22:18:55 -0400

    add options to cors

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', // process.env.NODE_ENV === 'local' ? ['http://localhost:3000'] : ['https://sage-space.jimmyleespann.com'],
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  // credentials: true, // allows the session cookie to be sent back and forth from server to client
+  optionsSuccessStatus: 200 // some legacy browsers choke on satus 204
+}));
 
 // middleware - JSON parsing
 app.use(express.urlencoded({extended: false}));
