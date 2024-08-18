3261e5a83c21eb36bfac634a4ff5d036eac1961f
Jimmy Spann <jimmyleespann@outlook.com>
2024-08-18 00:50:25 -0400

    remove expiration on token

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,10 +74,10 @@ const login = async (req, res) => {
       email: foundUser.email,
     };
     const secret = process.env.JWT_SECRET;
-    const expiration = {expiresIn: "3h"};
+    // const expiration = {expiresIn: "3h"};
     
     // SIGN TOKEN
-    const token = await jwt.sign(payload, secret, expiration);
+    const token = await jwt.sign(payload, secret /*, expiration */);
     
     const user = {
       token,
